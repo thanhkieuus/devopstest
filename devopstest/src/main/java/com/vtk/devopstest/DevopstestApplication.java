60985b622753af90048a03f9bc075467fda7e249
thanhkieuus <voltakieu@yahoo.com>
2018-05-27 02:07:36 -0500

    Added project packages

---
+++ devopstest/src/main/java/com/vtk/devopstest/DevopstestApplication.java
@@ -0,0 +1,12 @@
+package com.vtk.devopstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopstestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopstestApplication.class, args);
+	}
+}

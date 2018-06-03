38db7c5290d8754a29c2c47ef86556848e8b7d8b
thanhkieuus <voltakieu@yahoo.com>
2018-06-03 01:17:06 -0500

    Added  payload page

---
+++ devopstest/src/main/java/com/vtk/devopstest/web/controllers/PayloadController.java
@@ -0,0 +1,23 @@
+/**
+ *
+ */
+package com.vtk.devopstest.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author VK
+ *
+ */
+@Controller
+public class PayloadController {
+	
+	private static final String PAYLAAD_VIEW_NAME = "payload/payload";
+
+	@RequestMapping("/payload")
+	public String payload() {
+		return PAYLAAD_VIEW_NAME;
+	}
+
+}

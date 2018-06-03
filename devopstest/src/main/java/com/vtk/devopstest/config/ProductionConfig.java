e48fd907bdbaabb4ea9d6d7728f71c303658e5bf
thanhkieuus <voltakieu@yahoo.com>
2018-06-03 01:01:41 -0500

    Added login form

--- devopstest/src/main/java/com/vtk/devopstest/config/ProductionConfig.java
+++ devopstest/src/main/java/com/vtk/devopstest/config/ProductionConfig.java
@@ -3,13 +3,10 @@
  */
 package com.vtk.devopstest.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
-
 import com.vtk.devopstest.backend.service.EmailService;
 import com.vtk.devopstest.backend.service.SmtpMailService;
 
---
+++ devopstest/src/main/java/com/vtk/devopstest/web/controllers/LoginController.java
@@ -0,0 +1,31 @@
+/**
+ *
+ */
+package com.vtk.devopstest.web.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author VK
+ *
+ */
+@Controller
+public class LoginController {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(LoginController.class);
+	
+	
+	public static final String LOGIN_VIEW_NAME = "user/login";
+	
+	@RequestMapping("/login")
+	public String login() {
+		
+		LOG.info("************ Login...");
+		return LOGIN_VIEW_NAME;
+		
+	}
+
+}
